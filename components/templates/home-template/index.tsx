743bab27941d68d928e237a79517af5e8cb800bd
nidhal <nidhal@codinglab.com>
2022-02-06 16:33:30 +0100

    feat: add body bg color

--- components/templates/home-template/index.tsx
+++ components/templates/home-template/index.tsx
@@ -2,11 +2,9 @@ import NavBar from "../../organisms/navbar";
 
 const HomeTemplate = ({ children }) => {
   return (
-    <div className="flex flex-col items-center w-full h-screen">
-      <div className="w-full lg:w-3/4 2xl:w-1/2 px-6">
-        <NavBar></NavBar>
-        <div className="w-full">{children}</div>
-      </div>
+    <div className="flex flex-col items-center w-full h-screen bg-gray-50">
+      <NavBar></NavBar>
+      <div className="w-full lg:w-3/4 2xl:w-1/2 h-full p-6">{children}</div>
     </div>
   );
 };
